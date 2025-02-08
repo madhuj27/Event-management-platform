e3f9d7f6efeb20575f1802758445ff5073afc213
madhuj27 <147175568+madhuj27@users.noreply.github.com>
2025-02-08 23:17:05 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
    cors({
       credentials: true,
-      origin: "http://localhost:5173",
+      origin: "https://event-management-platform-z4nc.vercel.app/",
    })
 );
 
